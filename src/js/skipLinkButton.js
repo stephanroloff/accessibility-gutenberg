27c48a357dddad0d80a6ec470e31e78e8dca9642
Stephan Roloff <sr@werbeagenten.de>
2024-11-20 12:49:36 +0100

    skip link improvements

--- src/js/skipLinkButton.js
+++ src/js/skipLinkButton.js
@@ -8,6 +8,14 @@ let mainContentStart = document.querySelector('#main-content');
 if(mainContentStart){
     if(!mainContentStart.hasAttribute('tabindex') || element.tagName !== 'BUTTON' || element.tagName !== 'A'){
         mainContentStart = document.querySelector('#main-content a');
+       
+        if(!mainContentStart){
+            mainContentStart = document.querySelector('#main-content [tabindex]');
+
+            if(!mainContentStart){
+                mainContentStart = document.querySelector('#main-content button');
+            }
+        }
     }
     newLink.addEventListener('keydown', (event)=>{
         if (event.key === 'Enter') { 
